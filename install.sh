98232c10828ca44aab9360eff8d9830b530a2ac5
Sebastian Klose <mail@sklose.com>
2015-09-26 14:31:56 -0400

    Combine install.sh adn docker_compose_installer.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,11 @@ if [ -z $INSTANCE_ID ]; then
   exit 1
 fi
 
+if [ -z $AWS_S3_PREFIX ]; then
+  echo "Please set AWS_S3_PREFIX before running this script"
+  exit 1
+fi
+
 if [ -z $AWS_REGION ]; then
   AWS_REGION=$(wget -qO- $META_URL/placement/availability-zone)
   AWS_REGION=${AWS_REGION:0:-1}
@@ -36,20 +41,37 @@ if [ -z $ENV ]; then
   exit 1
 fi
 
-if [ -z $COMP_INSTALLER_URL ]; then
-  COMP_INSTALLER_URL=https://raw.githubusercontent.com/andasa-de/CloudInit/master/docker_compose_installer.sh
+if [ -z $INSTALL_DIR ]; then
+  INSTALL_DIR=/components
 fi
+
+mkdir -p $INSTALL_DIR
 
 logger -s Initializing Instance $INSTANCE_ID in $AWS_REGION
 
-export AWS_REGION
+function docker-compose-install {
+  COMP=$1
+  ENG=https://raw.githubusercontent.com/andasa-de/CloudInit/master/j2render.py
+  TMP=https://raw.githubusercontent.com/andasa-de/CloudInit/master/initd.j2
+  SRC=$AWS_S3_PREFIX/$ENV/$COMP.yml
+  DST=$INSTALL_DIR/$COMP.yml
+
+  wget -qO- $ENG > j2render.py
+  python j2render.py $TMP --component=$COMP --components_path=$INSTALL_DIR --region=$AWS_REGION --yml_src=$SRC > /etc/init.d/$COMP
+  chmod +x /etc/init.d/$COMP
+  update-rc.d $COMP defaults
+
+  /etc/init.d/$COMP update
+  /etc/init.d/$COMP start
+}
+
 aws ec2 describe-tags \
   --filter Name=resource-id,Values=$INSTANCE_ID Name=key,Values=$COMP_TAG_KEY \
   --region $AWS_REGION \
   --output text | grep -o '[[:alnum:]-]*' | tail -n+5 | while read -r COMP;
 do
   logger -s Installing $COMP...
-  wget -qO- $COMP_INSTALLER_URL | bash -s $COMP $ENV
+  docker-compose-install $COMP
   if [ $? != 0 ]; then
     logger -s Error while installing $COMP
     exit $?
